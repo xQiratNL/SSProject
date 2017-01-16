aacc136007484cd9751a0906df2afc84fdcc52bd
Tariq Bontekoe <thbontekoe@hotmail.com>
2017-01-16 16:41:08 +0100

    important functions added to game and player

--- src/connectfour/Game.java
+++ src/connectfour/Game.java
@@ -3,4 +3,28 @@ package connectfour;
 public class Game {
 
 	//TODO: add hasWinner and GameOver (from tictactoe board).
+	
+	private Board board;
+	private Player[] players;
+	private int currentPlayer;
+	
+	public Game(Player[] players) {
+		board = new Board();
+		this.players = players;
+		currentPlayer = 0;
+	}
+	
+	public boolean hasWinner() {
+		for (Player player: players) {
+			if (board.isWinner(player.getMark())) {
+				return true;
+			}
+		}
+		//no winner
+		return false;
+	}
+	
+	public boolean gameOver() {
+		return board.isFull() || this.hasWinner();
+	}
 }
--- src/connectfour/Player.java
+++ src/connectfour/Player.java
@@ -2,4 +2,21 @@ package connectfour;
 
 public abstract class Player {
 
+	private String name;
+	private Mark mark;
+	
+	public Player(String name, Mark mark) {
+		this.name = name;
+		this.mark = mark;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Mark getMark() {
+		return mark;
+	}
+	
+	//TODO: implement determineMove and makeMove
 }
