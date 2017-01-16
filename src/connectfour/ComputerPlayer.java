2a2ba24124243b36f2c5e85b0d3318f4332208d3
Tariq Bontekoe <thbontekoe@hotmail.com>
2017-01-16 16:47:40 +0100

    added constructors

--- src/connectfour/ComputerPlayer.java
+++ src/connectfour/ComputerPlayer.java
@@ -2,4 +2,9 @@ package connectfour;
 
 public class ComputerPlayer extends Player {
 
+	public ComputerPlayer(String name, Mark mark) {
+		super(name, mark);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- src/connectfour/HumanPlayer.java
+++ src/connectfour/HumanPlayer.java
@@ -2,4 +2,9 @@ package connectfour;
 
 public class HumanPlayer extends Player {
 
+	public HumanPlayer(String name, Mark mark) {
+		super(name, mark);
+		// TODO Auto-generated constructor stub
+	}
+
 }
