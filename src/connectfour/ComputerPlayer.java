302c712fbd29cf3c9b4132a72c66f51c97b4198d
Rutger <groen@friesappeltje.nl>
2017-01-17 11:40:30 +0100

    Changed Players
    Added ComputerPlayer;
    Changed Player;
    Changed Strategy.

--- src/connectfour/ComputerPlayer.java
+++ src/connectfour/ComputerPlayer.java
@@ -2,15 +2,28 @@ package connectfour;
 
 public class ComputerPlayer extends Player {
 
-	public ComputerPlayer(String name, Mark mark) {
-		super(name, mark);
-		// TODO Auto-generated constructor stub
+	Strategy strategy;
+	Mark mark;
+	
+    // -- Constructor -----------------------------------------------
+    /**
+     * Creates a new ComputerPlayer object.
+     * 
+     */	
+	public ComputerPlayer(String name, Mark mark, Strategy strategy) {
+		super(name, mark, strategy);
+		this.strategy = strategy;
+		this.mark = mark;
 	}
 
+    /**
+     * Determines the field for the next move.
+     * 
+     * @param board the current game board
+     * @return the player's choice
+     */
 	@Override
 	public int determineMove(Board board) {
-		// TODO Auto-generated method stub
-		return 0;
+		return strategy.determineMove(board, mark);
 	}
-
 }
--- src/connectfour/Strategy.java
+++ src/connectfour/Strategy.java
@@ -1,5 +1,9 @@
 package connectfour;
 
 public interface Strategy {
+	
+	public String getName();
 
+	public int determineMove(Board board, Mark mark);
+	
 }
