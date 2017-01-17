96b54e6b6784a8273180d9f43c86e58a7ab7621d
Tariq Bontekoe <thbontekoe@hotmail.com>
2017-01-17 14:47:36 +0100

    client and server no longer extend peer

--- src/connectfour/Client.java
+++ src/connectfour/Client.java
@@ -1,5 +1,5 @@
 package connectfour;
 
-public class Client extends Peer {
+public class Client {
 
 }
--- src/connectfour/Server.java
+++ src/connectfour/Server.java
@@ -1,5 +1,5 @@
 package connectfour;
 
-public class Server extends Peer {
+public class Server {
 
 }
