d4036b705384659bd6d63e43c3746ea778ba68bb
Rutger <groen@friesappeltje.nl>
2017-01-17 12:04:32 +0100

    Added Dummy SmartStrategy

---
+++ src/connectfour/SmartStrategy.java
@@ -0,0 +1,44 @@
+package connectfour;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SmartStrategy implements Strategy {
+
+	String name;
+	String[] computerNames = {"Tariq", "Rutger"};
+	
+	public SmartStrategy() {
+		this.name = computerNames[(int) (Math.random()*computerNames.length)];
+	}
+	
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public int determineMove(Board board, Mark mark) {
+		Set<Integer> set = new HashSet<Integer>();
+		// Add all empty fields to a set.
+		for (int i = 0; i < board.getSize(); i++) {
+			if (board.isEmptyField(i)) {
+				set.add(i);
+			}
+		}
+		
+		// Pick a random field from the set (thus a random empty field)
+		int setMove = 0;
+		int r = (int) (Math.random() * set.size());
+		int i = 0;
+		for (Integer s : set) {
+			if (r == i) {
+		    	setMove = s;
+		    	break;
+		    }
+			i++;
+		}
+		return setMove;
+	}
+
+}
