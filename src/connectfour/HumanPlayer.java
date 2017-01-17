24d31008ad796e5ef2c7cced6f9b8819d9716b91
Rutger <groen@friesappeltje.nl>
2017-01-17 11:17:37 +0100

    Added HumanPlayer
    implemented

--- src/connectfour/HumanPlayer.java
+++ src/connectfour/HumanPlayer.java
@@ -1,10 +1,75 @@
 package connectfour;
 
+import java.util.Scanner;
+
 public class HumanPlayer extends Player {
 
-	public HumanPlayer(String name, Mark mark) {
-		super(name, mark);
-		// TODO Auto-generated constructor stub
-	}
+    // -- Constructors -----------------------------------------------
+
+    /*@
+       requires name != null;
+       requires mark != null;
+       ensures this.getName() == name;
+       ensures this.getMark() == mark;
+    */
+    /**
+     * Creates a new human player object.
+     * 
+     */
+    public HumanPlayer(String name, Mark mark) {
+        super(name, mark);
+    }
+
+    // -- Commands ---------------------------------------------------
+
+    /*@
+       requires board != null;
+       ensures board.isField(\result) && board.isEmptyField(\result);
+
+     */
+    /**
+     * Asks the user to input the field where to place the next mark. This is
+     * done using the standard input/output. \
+     * 
+     * @param board the game board
+     * @return the player's chosen field
+     */
+    public int determineMove(Board board) {
+        String prompt = "> " + getName() + " (" + getMark().toString() + ")"
+                + ", what is your choice? ";
+        int choice = readInt(prompt);
+        boolean valid = board.isField(choice) && board.isEmptyField(choice);
+        while (!valid) {
+            System.out.println("ERROR: field " + choice
+                    + " is no valid choice.");
+            choice = readInt(prompt);
+            valid = board.isField(choice) && board.isEmptyField(choice);
+        }
+        return choice;
+    }
+
+    /**
+     * Writes a prompt to standard out and tries to read an int value from
+     * standard in. This is repeated until an int value is entered.
+     * 
+     * @param prompt the question to prompt the user
+     * @return the first int value which is entered by the user
+     */
+    private int readInt(String prompt) {
+        int value = 0;
+        boolean intRead = false;
+        @SuppressWarnings("resource")
+        Scanner line = new Scanner(System.in);
+        do {
+            System.out.print(prompt);
+            try (Scanner scannerLine = new Scanner(line.nextLine());) {
+                if (scannerLine.hasNextInt()) {
+                    intRead = true;
+                    value = scannerLine.nextInt();
+                }
+            }
+        } while (!intRead);
+        return value;
+    }
 
 }
