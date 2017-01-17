9fed71e22140f0209a430b60f823d6e8607ed7f4
Rutger <groen@friesappeltje.nl>
2017-01-17 11:18:09 +0100

    Added Override ComputerPlayer

--- src/connectfour/ComputerPlayer.java
+++ src/connectfour/ComputerPlayer.java
@@ -7,4 +7,10 @@ public class ComputerPlayer extends Player {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public int determineMove(Board board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
