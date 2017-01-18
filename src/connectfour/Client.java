d6baba9031a1e26b0239b673f9a07457dcdf83e7
Rutger <groen@friesappeltje.nl>
2017-01-18 11:49:25 +0100

    Added Client and a TUI for the client

--- src/connectfour/Client.java
+++ src/connectfour/Client.java
@@ -1,8 +1,48 @@
 package connectfour;
 
-public class Client {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-	public static void main (String[] args) {
-		(new Client()).start();
-	}
+public class Client {
+    private static final String USAGE
+    = "usage: java connectfour.Client <address>";
+
+    /** Starts a Client application. */
+    public static void main(String[] args) {
+    	if (args.length != 1) {
+    		System.out.println(USAGE);
+    		System.exit(0);
+    	}
+
+    	InetAddress addr = null;
+    	Socket sock = null;
+    	
+    	// check args[0] - the IP-adress
+    	try {
+    		addr = InetAddress.getByName(args[0]);
+    	} catch (UnknownHostException e) {
+    		System.out.println(USAGE);
+    		System.out.println("ERROR: host " + args[1] + " unknown");
+    		System.exit(0);
+    	}
+
+    	// try to open a Socket to the server
+    	try {
+    		sock = new Socket(addr, Protocol.PORTNUMBER);
+    	} catch (IOException e) {
+    		System.out.println("ERROR: could not create a socket on " + addr
+    				+ " and port " + Protocol.PORTNUMBER);
+    	}
+
+    	//TODO: Socket >> TUI
+    	
+        // create Peer object and start the two-way communication
+    	ClientTui tui = new ClientTui(sock);
+        Thread streamInputHandler = new Thread(tui);
+        streamInputHandler.start();
+        tui.handleTerminalInput();
+        tui.shutDown();
+    }
 }
---
+++ src/connectfour/ClientTui.java
@@ -0,0 +1,88 @@
+package connectfour;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class ClientTui implements Runnable {
+	BufferedReader in;
+	BufferedWriter out;
+	Socket sock;
+	
+	public ClientTui(Socket sock) {
+    	try {
+			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	this.sock = sock;
+    	
+	}
+
+	@Override
+	public void run() {
+    	while (!sock.isClosed()) {
+    		try {
+				if (in.ready()) {
+					System.out.println(in.readLine());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+	}
+	
+    /**
+     * Reads a string from the console and sends this string over
+     * the socket-connection to the Peer process.
+     * On Peer.EXIT the method ends
+     */
+    public void handleTerminalInput() {
+		String input = "";
+    	while (!input.equals("exit")) {
+			input = readString(": ");
+			try {
+				if (!input.equals("exit")) {
+					out.write(input);
+					out.newLine();
+					out.flush();
+					System.out.println("output has been written (" + input + ")");
+				}
+			} catch (IOException e) {
+				System.out.println("ERR: Socket is closed!");
+			}
+		}
+    }
+    	
+    /**
+     * Closes the connection, the sockets will be terminated.
+     */
+    public void shutDown() {
+		try {
+			sock.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+    }
+    
+    /** read a line from the default input */
+    static public String readString(String tekst) {
+        System.out.print(tekst);
+        String antw = null;
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    System.in));
+            antw = in.readLine();
+        } catch (IOException e) {
+        }
+
+        return (antw == null) ? "" : antw;
+    }
+}
