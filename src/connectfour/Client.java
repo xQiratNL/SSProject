cbe4a1969937fd762f0346be21470b3a9724a079
Rutger <groen@friesappeltje.nl>
2017-01-23 14:37:26 +0100

    Updated client

--- src/connectfour/Client.java
+++ src/connectfour/Client.java
@@ -1,30 +1,37 @@
 package connectfour;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
     private static final String USAGE
     = "usage: java connectfour.Client <address>";
 
-    /** Starts a Client application. */
-    public static void main(String[] args) {
-    	if (args.length != 1) {
-    		System.out.println(USAGE);
-    		System.exit(0);
-    	}
-
+    public Client(String InetAdress) {
+    	try {
+			this.start(InetAdress);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public void start(String InetAdress) throws IOException {
     	InetAddress addr = null;
     	Socket sock = null;
+    	BufferedReader in;
     	
     	// check args[0] - the IP-adress
     	try {
-    		addr = InetAddress.getByName(args[0]);
+    		addr = InetAddress.getByName(InetAdress);
     	} catch (UnknownHostException e) {
     		System.out.println(USAGE);
-    		System.out.println("ERROR: host " + args[1] + " unknown");
+    		System.out.println("ERROR: host " + InetAdress + " unknown");
     		System.exit(0);
     	}
 
@@ -37,11 +44,119 @@ public class Client {
     	}
     	
         // create ClientTui object in a new thread and start the two-way communication.
-    	// The new thread is created to maintain two process at a time. The output and input.
+    	// The new thread is created to maintain one process: the output.
     	ClientTui tui = new ClientTui(sock);
         Thread streamInputHandler = new Thread(tui);
         streamInputHandler.start();
-        tui.handleTerminalInput();
-        tui.shutDown();
+        
+        // Handle all incoming communication: the input.
+        in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		while (!sock.isClosed()) {
+			try {
+				if (in.ready()) {	
+					processInput(in.readLine());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}   	
     }
+    
+    
+    /**
+     * Method for processing input from the server.
+     * 
+     * @param input The input message from the server.
+     */
+    public void processInput(String input) {
+    	Scanner scanner = new Scanner(input);
+    	String command = scanner.next();
+    	System.out.println(input);
+    	switch (command) {
+    		
+    		// Connect to a server:
+    		case Protocol.HELLO:
+    			//do this
+    			break;
+    		case Protocol.ERROR_USERNAMETAKEN:
+    			//do this
+    			break;
+    		
+    		// Starting a game
+    		case Protocol.WAIT:
+    			//do this
+    			break;
+    		case Protocol.READY:
+    			//do this
+    			break;
+    		
+    		// Playing a game
+    		case Protocol.REQUESTMOVE:
+    			//do this
+    			break;
+    		case Protocol.SETMOVE:
+    			//do this
+    			break;
+    		case Protocol.ERROR_INVALIDMOVE:
+    			//do this
+    			break;
+    		case Protocol.ERROR_NOTYOURTURN:
+    			//do this
+    			break;
+    		case Protocol.GAMEOVER:
+    			//do this
+    			break;
+    		case Protocol.ERROR_USERQUIT:
+    			//do this
+    			break;
+    		
+    			
+    		// ====== OPTIONALS ======
+    		// Chat
+    		case Protocol.BROADCAST:
+    			//do this
+    			break;
+    		case Protocol.WHISPER:
+    			//do this
+    			break;
+    		case Protocol.CHATUSER:
+    			//do this
+    			break;
+    		case Protocol.GAMECHAT:
+    			//do this
+    			break;
+    		case Protocol.ERROR_USER_HAS_NO_CHAT:
+    			//do this
+    			break;
+    		case Protocol.ERROR_USER_NOT_FOUND:
+    			//do this
+    			break;
+    		case Protocol.ERROR_NOT_IN_GAME:
+    			//do this
+    			break;
+    		
+    		// Challenge
+    		
+    		// Leaderboard
+    			
+    		// Password
+    		
+    		// Other
+    		case Protocol.ERROR_COMMAND_NOT_RECOGNIZED:
+    			System.out.println("Command not recognized!");
+    			//TODO: make usage function
+    			break;
+    	}
+    }
+    
+    /** Starts a Client application. */
+    public static void main(String[] args) {
+    	if (args.length != 1) {
+    		System.out.println(USAGE);
+    		System.exit(0);
+    	}
+    	new Client(args[0]);
+    }
+    
 }
--- src/connectfour/ClientTui.java
+++ src/connectfour/ClientTui.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ClientTui implements Runnable {
-	BufferedReader in;
+	// BufferedReader in;
 	BufferedWriter out;
 	Socket sock;
 	
@@ -23,7 +22,7 @@ public class ClientTui implements Runnable {
      */
 	public ClientTui(Socket sock) {
     	try {
-			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			// in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -33,27 +32,14 @@ public class ClientTui implements Runnable {
     	
 	}
 
-	
-	@Override
-	public void run() {
-    	while (!sock.isClosed()) {
-    		try {
-				if (in.ready()) {	
-					processInput(in.readLine());
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    	}
-	}
-	
+
 	/**
      * Reads a string from the console and sends this string over
      * the socket-connection to the ClientTui process.
      * On "exit" the method ends
      */
-    public void handleTerminalInput() {
+	@Override
+	public void run() {
 		String input = "";
     	while (!input.equals("exit")) {
 			input = readString();
@@ -68,7 +54,8 @@ public class ClientTui implements Runnable {
 				System.out.println("ERR: Socket is closed!");
 			}
 		}
-    }
+	}
+	
     	
     /**
      * Closes the connection, the sockets will be terminated.
@@ -96,94 +83,6 @@ public class ClientTui implements Runnable {
         }
 
         return (antw == null) ? "" : antw;
-    }
-    
-    
-    /**
-     * Method for processing input from the server.
-     * 
-     * @param input The input message from the server.
-     */
-    private void processInput(String input) {
-    	Scanner scanner = new Scanner(input);
-    	String command = scanner.next();
-    	System.out.println(input);
-    	//TODO: Add methods or functions, (VAN TARIQ: je kan beter naar de static variabele verwijzen, ipv de string, dit voorkomt problemen als het protocol wordt gewijzigd.
-    	switch (command) {
-    		
-    		// Connect to a server:
-    		case "HELLO":
-    			//do this
-    			break;
-    		case "ERR_USERNAME_TAKEN":
-    			//do this
-    			break;
-    		
-    		// Starting a game
-    		case "WAIT":
-    			//do this
-    			break;
-    		case "READY":
-    			//do this
-    			break;
-    		
-    		// Playing a game
-    		case "REQUESTMOVE":
-    			//do this
-    			break;
-    		case "SETMOVE":
-    			//do this
-    			break;
-    		case "ERR_INVALIDMOVE":
-    			//do this
-    			break;
-    		case "ERR_NOTYOURTURN":
-    			//do this
-    			break;
-    		case "GAMEOVER":
-    			//do this
-    			break;
-    		case "ERR_USERQUIT":
-    			//do this
-    			break;
-    		
-    			
-    		// ====== OPTIONALS ======
-    		// Chat
-    		case "BROADCAST":
-    			//do this
-    			break;
-    		case "WHISPER":
-    			//do this
-    			break;
-    		case "CHATUSERS":
-    			//do this
-    			break;
-    		case "GAMECHAT":
-    			//do this
-    			break;
-    		case "ERR_ISERNOTFOUNDCHAT":
-    			//do this
-    			break;
-    		case "ERR_USERHASNOCHAT":
-    			//do this
-    			break;
-    		case "ERR_NOTINAGAME":
-    			//do this
-    			break;
-    		
-    		// Challenge
-    		
-    		// Leaderboard
-    			
-    		// Password
-    		
-    		// Other
-    		case "ERR_COMMANDNOTRECOGNIZED":
-    			//do this
-    			break;
-    	}
-    }
-    
+    }   
     
 }
